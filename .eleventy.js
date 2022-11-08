c76a965a6b09ec51235ae649e27cc69c0c73e0b1
aik6980 <aik6980>
2022-11-08 10:04:39 +0000

    add pathPrefix

--- .eleventy.js
+++ .eleventy.js
@@ -3,6 +3,7 @@ module.exports = function(eleventyConfig) {
 	eleventyConfig.addPassthroughCopy("src/resume.css");
 	
 	return {
+		pathPrefix: "/resume/"
 		dir : {
 			input : "src",
 			includes : "_includes"
